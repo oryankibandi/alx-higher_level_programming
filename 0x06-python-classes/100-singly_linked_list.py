a2150456b1c50a3eed6499d1db457eaa5d561b93
oryankibandi <shreckoryan@gmail.com>
2022-06-23 02:44:23 +0300

    fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,17 +6,31 @@ This module defines a Singly linked list
 
 class Node:
     def __init__(self, data, next_node=None):
-        """Defines a node for a singly linked list
+        """
+        Defines a node for a singly linked list
+
+        Args:
+            data (int): data of  the node
+            next_node (Node): pointer to the next node
         """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        data getter
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        setter for the data attribute
+
+        Args:
+            value (int): value to be set
+        """
         if type(value) != int:
             raise TypeError('data must be an integer')
 
@@ -24,10 +38,17 @@ class Node:
 
     @property
     def next_node(self):
+        """getter for the next node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        setter for the next node
+
+        Args:
+            value (node): next node
+        """
         if value is not None and type(value) != Node:
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
@@ -40,6 +61,12 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        inserts a new node in the linked list
+
+        Args:
+            value (Node): node to insert
+        """
         if self.__head is None:
             self.__head = Node(value)
         else:
@@ -57,6 +84,9 @@ class SinglyLinkedList:
                 previous.next_node = newNode
 
     def __repr__(self):
+        """
+        returns data as string
+        """
         node = self.__head
         text = ''
         while 1:
