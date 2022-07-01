28a469c37021dea266fe738bfb6fb639f78267c9
oryankibandi <shreckoryan@gmail.com>
2022-07-01 23:21:02 +0300

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,24 +10,23 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class MaxIntegerTest(unittest.TestCase):
-    """
-    This class contains tests for the module
-    """
+class TestMaxInteger(unittest.TestCase):
+    def test_positive(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([5, 2, 3, 4]), 5)
+        self.assertEqual(max_integer([1.10, 2.20, 6.60, 4.2]), 6.60)
+        self.assertEqual(max_integer([0, 0, 0, 0.000]), 0)
 
-    def test_len(self):
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -22, -3.44, -4.111]), -1)
+        self.assertEqual(max_integer([-1, -2, -3, -0]), 0)
+
+    def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_no_arg(self):
+    def test_one_element(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_no_args(self):
         self.assertEqual(max_integer(), None)
-
-    def test_pos(self):
-        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertEqual(max_integer([7, 35, 65, 48]), 65)
-
-    def test_neg(self):
-        self.assertEqual(max_integer([-4, -52, -96, -2]). -2)
-        self.assertEqual(max_integer([-54, -25, -35, -98]), -25)
-
-    def test_single_elem(self):
-        self.assertEqual(max_integer([2]), 2)
